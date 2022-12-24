5c5e29e83f7f1f211e3220c852241c749d8a6c87
ashley <ashleylai58@gmail.com>
2022-12-24 15:07:02 +0800

    finish stock list

--- stock-fe/src/components/Stock.js
+++ stock-fe/src/components/Stock.js
@@ -41,10 +41,10 @@ const Stock = () => {
 
       {stocks.map((stock) => {
         return (
-          <div className="bg-white bg-gray-50 p-6 rounded-lg shadow hover:shadow-lg m-6 cursor-pointer">
+          <div key={stock.id} className="bg-white bg-gray-50 p-6 rounded-lg shadow hover:shadow-lg m-6 cursor-pointer">
             <Link to={'/stock/1234'}>
-              <h2 className="text-2xl font-bold mb-2 text-gray-800">股票代碼</h2>
-              <p className="text-gray-700">股票名稱</p>
+              <h2 className="text-2xl font-bold mb-2 text-gray-800">{stock.id}</h2>
+              <p className="text-gray-700">{stock.name}</p>
             </Link>
           </div>
         );
